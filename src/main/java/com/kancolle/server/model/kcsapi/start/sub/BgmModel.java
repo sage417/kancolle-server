c970eb6586b9e1451d26f47120f167719e3224e2
J.K.SAGE <sage417@foxmail.com>
2015-04-24 14:25:24 +0900

    Update: update Bgm Model

--- src/main/java/com/kancolle/server/model/kcsapi/start/sub/BgmModel.java
+++ src/main/java/com/kancolle/server/model/kcsapi/start/sub/BgmModel.java
@@ -1,14 +1,20 @@
 package com.kancolle.server.model.kcsapi.start.sub;
 
+import com.kancolle.server.mapper.annotation.Column;
+import com.kancolle.server.mapper.annotation.Table;
+
+@Table("t_bgm")
 public class BgmModel {
 
     private int api_id;
+
     private String api_name;
 
     public int getApi_id() {
         return api_id;
     }
 
+    @Column(name = "ID", type = int.class)
     public void setApi_id(int api_id) {
         this.api_id = api_id;
     }
@@ -17,6 +23,7 @@ public class BgmModel {
         return api_name;
     }
 
+    @Column(name = "NAME", type = String.class)
     public void setApi_name(String api_name) {
         this.api_name = api_name;
     }
