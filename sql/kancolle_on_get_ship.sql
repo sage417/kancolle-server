82f813b8116ec83b4ee5a4bf80bea956f68b2a4e
J.K.SAGE <sage417@foxmail.com>
2015-04-28 18:32:47 +0900

    Fix: 修正sql错误

--- sql/kancolle_on_get_ship.sql
+++ sql/kancolle_on_get_ship.sql
@@ -8,11 +8,11 @@ CREATE
     /*!50017 DEFINER = 'root'@'localhost' */
     TRIGGER `tri_on_get_ship` BEFORE INSERT ON `t_member_ship` 
     FOR EACH ROW BEGIN
+	DECLARE counter_v BIGINT;
+	
 	UPDATE	t_member SET member_ship_counter=member_ship_counter+1 WHERE member_id = new.member_id;
-
-	DECLARE v_counter BIGINT;
-	SET v_counter = (SELECT member_ship_counter FROM t_member WHERE member_id = new.member_id);
-	SET new.ID = v_counter;
+	SET counter_v = (SELECT member_ship_counter FROM t_member WHERE member_id = new.member_id);
+	SET new.ID = counter_v;
     END;
 $$
 
