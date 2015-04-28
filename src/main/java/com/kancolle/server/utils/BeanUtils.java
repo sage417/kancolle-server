4aa5e53b59f857f037a57f65b20fd45c2bb28bb0
J.K.SAGE <sage417@foxmail.com>
2015-04-28 10:07:04 +0900

    Update: 重构工具类

--- src/main/java/com/kancolle/server/utils/BeanUtils.java
+++ src/main/java/com/kancolle/server/utils/BeanUtils.java
@@ -5,19 +5,21 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class BeanUtils {
+	public static final String STR_SET = "set";
+	public static final String STR_GET = "get";
 
-    private BeanUtils() {
-    }
+	private BeanUtils() {
+	}
 
-    public static <T> Stream<Method> getGetMethodStream(Class<T> clazz, Class<?> returnType) {
-        return getMethodStream(clazz).filter(method -> method.getName().startsWith("get") && returnType.equals(method.getReturnType()));
-    }
+	public static <T> Stream<Method> getGetMethodStream(Class<T> clazz, Class<?> returnType) {
+		return getMethodStream(clazz).filter(method -> method.getName().startsWith(STR_GET) && method.getParameterCount() == 0 && returnType.equals(method.getReturnType()));
+	}
 
-    public static <T> Stream<Method> getSetMethodStream(Class<T> clazz, Class<?> paramType) {
-        return getMethodStream(clazz).filter(method -> method.getName().startsWith("set") && method.getParameterCount() == 1 && paramType.equals(method.getParameterTypes()[0]));
-    }
+	public static <T> Stream<Method> getSetMethodStream(Class<T> clazz, Class<?> paramType) {
+		return getMethodStream(clazz).filter(method -> method.getName().startsWith(STR_SET) && method.getParameterCount() == 1 && paramType.equals(method.getParameterTypes()[0]));
+	}
 
-    public static <T> Stream<Method> getMethodStream(Class<T> clazz) {
-        return Arrays.asList(clazz.getMethods()).stream();
-    }
+	public static <T> Stream<Method> getMethodStream(Class<T> clazz) {
+		return Arrays.asList(clazz.getMethods()).stream();
+	}
 }
--- src/main/java/com/kancolle/server/utils/DaoUtils.java
+++ src/main/java/com/kancolle/server/utils/DaoUtils.java
@@ -1,6 +1,8 @@
 package com.kancolle.server.utils;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.function.Function;
@@ -13,77 +15,91 @@ import com.kancolle.server.dao.base.BaseDao;
 import com.kancolle.server.mapper.annotation.Column;
 
 public class DaoUtils {
-    private static final String STR_SET = "set";
-    private static final String STR_GET = "get";
-    private static final String PARAM_PREFIX = "api";
+	private static final String PARAM_PREFIX = "api";
 
-    private static final Pattern METHOD_NAME_PATTERN_REPLACER = Pattern.compile("_[a-zA-Z0-9]");
+	private static final Pattern METHOD_NAME_PATTERN_REPLACER = Pattern.compile("_[a-zA-Z0-9]");
 
-    private static final Predicate<Method> IS_SET_METHOD = method -> method.getName().startsWith(STR_SET) && method.getParameterCount() == 1;
+	private static final Predicate<Method> IS_SET_METHOD = method -> method.getName().startsWith(BeanUtils.STR_SET) && method.getParameterCount() == 1;
 
-    private static final Function<String, String> GET_PARAM_NAME = method_name -> {
-        String raw_name = method_name.substring(STR_SET.length() + PARAM_PREFIX.length());
-        Matcher matcher = METHOD_NAME_PATTERN_REPLACER.matcher(raw_name);
+	private static final Function<String, String> GET_PARAM_NAME = method_name -> {
+		String raw_name = method_name.substring(BeanUtils.STR_SET.length() + PARAM_PREFIX.length());
+		Matcher matcher = METHOD_NAME_PATTERN_REPLACER.matcher(raw_name);
 
-        while (matcher.find()) {
-            raw_name = matcher.replaceFirst(matcher.group().substring(1).toUpperCase());
-            matcher = METHOD_NAME_PATTERN_REPLACER.matcher(raw_name);
-        }
-        return STR_GET + raw_name;
-    };
+		while (matcher.find()) {
+			raw_name = matcher.replaceFirst(matcher.group().substring(1).toUpperCase());
+			matcher = METHOD_NAME_PATTERN_REPLACER.matcher(raw_name);
+		}
+		return BeanUtils.STR_GET + raw_name;
+	};
 
-    @SuppressWarnings("unchecked")
-    public static <T> T setObject(BaseDao<T> dao, Class<?>[] parameterTypes, Object[] parameters) throws InstantiationException, IllegalAccessException {
-    	
-    	Class<T >targetClass = (Class<T>) GenericsUtils.getSuperClassGenricType(dao.getClass(), 0);
-    	
-        T instance = targetClass.newInstance();
+	@SuppressWarnings("unchecked")
+	public static <T> T setObject(BaseDao<T> dao, Class<?>[] parameterTypes, Object[] parameters) throws InstantiationException, IllegalAccessException {
 
-        Arrays.asList(targetClass.getMethods()).stream().filter(IS_SET_METHOD).forEach(method -> {
-            try {
-                Method daoMethod = dao.getClass().getMethod(GET_PARAM_NAME.apply(method.getName()), parameterTypes);
-                method.invoke(instance, daoMethod.invoke(dao, parameters));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        return instance;
-    }
+		Class<T> targetClass = (Class<T>) getSuperClassGenricType(dao.getClass());
 
-    public static void setObject(Object target, ResultSet rs) {
-        Arrays.asList(target.getClass().getDeclaredMethods()).stream().filter(IS_SET_METHOD).forEach(method -> {
-            Column[] columms = method.getAnnotationsByType(Column.class);
-            if (columms.length == 0) {
-                System.out.println(method);
-                return;
-            }
+		T instance = targetClass.newInstance();
 
-            Column columm = columms[0];
+		Arrays.asList(targetClass.getMethods()).stream().filter(IS_SET_METHOD).forEach(method -> {
+			try {
+				Method daoMethod = dao.getClass().getMethod(GET_PARAM_NAME.apply(method.getName()), parameterTypes);
+				method.invoke(instance, daoMethod.invoke(dao, parameters));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		return instance;
+	}
 
-            String name = columm.name();
-            Class<?> type = columm.type();
-            try {
-                if (type == int.class) {
-                    method.invoke(target, rs.getInt(name));
-                    return;
-                }
-                if (type == String.class) {
-                    method.invoke(target, rs.getString(name));
-                    return;
-                }
-                if (type == long.class) {
-                    method.invoke(target, rs.getLong(name));
-                    return;
-                }
-                if (type == double.class) {
-                    method.invoke(target, rs.getDouble(name));
-                }
-                if (type == JSONObject.class) {
-                    method.invoke(target, rs.getString(name));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
+	public static Class<?> getSuperClassGenricType(Class<?> clazz) {
+		return getSuperClassGenricType(clazz, 0);
+	}
+
+	public static Class<?> getSuperClassGenricType(Class<?> clazz, int index) {
+		Type genType = clazz.getGenericSuperclass();
+
+		while (!(genType instanceof ParameterizedType)) {
+			clazz = clazz.getSuperclass();
+			genType = clazz.getGenericSuperclass();
+		}
+
+		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+		return (Class<?>) params[index];
+	}
+
+	public static void setObject(Object target, ResultSet rs) {
+		Arrays.asList(target.getClass().getDeclaredMethods()).stream().filter(IS_SET_METHOD).forEach(method -> {
+			Column[] columms = method.getAnnotationsByType(Column.class);
+			if (columms.length == 0) {
+				System.out.println(method);
+				return;
+			}
+
+			Column columm = columms[0];
+
+			String name = columm.name();
+			Class<?> type = columm.type();
+			try {
+				if (type == int.class) {
+					method.invoke(target, rs.getInt(name));
+					return;
+				}
+				if (type == String.class) {
+					method.invoke(target, rs.getString(name));
+					return;
+				}
+				if (type == long.class) {
+					method.invoke(target, rs.getLong(name));
+					return;
+				}
+				if (type == double.class) {
+					method.invoke(target, rs.getDouble(name));
+				}
+				if (type == JSONObject.class) {
+					method.invoke(target, rs.getString(name));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 }
