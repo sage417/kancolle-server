c545d49517ae562229830ccc0bc30da377c0dff0
J.K.SAGE <sage417@foxmail.com>
2015-04-28 18:27:00 +0900

    Add: 增加数据库脚本文件

---
+++ sql/kancolle_on_get_ship.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+USE `kancolle`$$
+
+DROP TRIGGER /*!50032 IF EXISTS */ `tri_on_get_ship`$$
+
+CREATE
+    /*!50017 DEFINER = 'root'@'localhost' */
+    TRIGGER `tri_on_get_ship` BEFORE INSERT ON `t_member_ship` 
+    FOR EACH ROW BEGIN
+	UPDATE	t_member SET member_ship_counter=member_ship_counter+1 WHERE member_id = new.member_id;
+
+	DECLARE v_counter BIGINT;
+	SET v_counter = (SELECT member_ship_counter FROM t_member WHERE member_id = new.member_id);
+	SET new.ID = v_counter;
+    END;
+$$
+
+DELIMITER ;
