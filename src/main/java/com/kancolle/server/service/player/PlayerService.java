dcb3e2b1cb36ebfa2e7f58e10936123be997bdb1
J.K.SAGE <sage417@foxmail.com>
2015-05-01 13:59:22 +0900

    Clean

--- src/main/java/com/kancolle/server/service/player/PlayerService.java
+++
@@ -1,8 +0,0 @@
-package com.kancolle.server.service.player;
-
-import com.kancolle.server.mapper.player.Player;
-
-public interface PlayerService {
-
-    public Player getPlayerByApiToken(String api_token);
-}
