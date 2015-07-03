2fa8f8ae0931503713b108e7afb05fddc6aea9d1
J.K.SAGE <J.K.SAGE@JKSAGE-PC>
2015-07-03 22:52:44 +0800

    Add:Model

---
+++ src/main/java/com/kancolle/server/controller/kcsapi/form/ship/ShipPowerUpForm.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.kancolle.server.controller.kcsapi.form.ship;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * @author J.K.SAGE
+ * @Date 2015年7月3日
+ *
+ */
+public class ShipPowerUpForm {
+
+    @NotNull
+    private Long api_id;
+
+    @NotEmpty
+    private List<Long> api_id_items;
+
+    public Long getApi_id() {
+        return api_id;
+    }
+
+    public void setApi_id(Long api_id) {
+        this.api_id = api_id;
+    }
+
+    public List<Long> getApi_id_items() {
+        return api_id_items;
+    }
+
+    public void setApi_id_items(List<Long> api_id_items) {
+        this.api_id_items = api_id_items;
+    }
+}
