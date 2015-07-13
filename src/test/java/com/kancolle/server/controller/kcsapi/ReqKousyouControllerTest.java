99a786dbc1e40f1257ba8d26425934b2d4c2b4bc
J.K.SAGE <J.K.SAGE@JKSAGE-PC>
2015-07-13 20:53:22 +0800

    Update

--- src/test/java/com/kancolle/server/controller/kcsapi/ReqKousyouControllerTest.java
+++ src/test/java/com/kancolle/server/controller/kcsapi/ReqKousyouControllerTest.java
@@ -5,8 +5,13 @@ package com.kancolle.server.controller.kcsapi;
 
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,7 +27,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 /**
  * @author J.K.SAGE
@@ -34,9 +42,12 @@ import org.springframework.web.context.WebApplicationContext;
 @ContextHierarchy({ @ContextConfiguration(name = "parent", locations = "classpath*:/spring/spring-context.xml"),
         @ContextConfiguration(name = "child", locations = "classpath*:/spring/mvc-context.xml") })
 public class ReqKousyouControllerTest {
+    @Autowired
+    DruidDataSource dataSource;
 
     @Autowired
     private WebApplicationContext wac;
+
     private MockMvc mockMvc;
 
     @Before
@@ -45,8 +56,10 @@ public class ReqKousyouControllerTest {
     }
 
     @After
-    public void restoreDb() {
-
+    public void restoreDb() throws SQLException, IOException {
+        ScriptRunner runner = new ScriptRunner(dataSource.getConnection());
+        runner.runScript(Files.newBufferedReader(ResourceUtils.getFile("classpath:kancolle.sql").toPath()));
+        runner.closeConnection();
     }
 
     public void testDestroyShip() {
@@ -60,8 +73,8 @@ public class ReqKousyouControllerTest {
     @Test
     public void testDestroyitem2() throws Exception {
         MvcResult result = mockMvc
-                .perform(MockMvcRequestBuilders.post("/kcsapi/api_req_kousyou/destroyitem2").param("api_token", "de1d61f922ae5604a0c479914813d8a18d5c9b6f").param("api_slotitem_ids", "1,2,3"))
+                .perform(MockMvcRequestBuilders.post("/kcsapi/api_req_kousyou/destroyitem2").param("api_token", "de1d61f922ae5604a0c479914813d8a18d5c9b6f").param("api_slotitem_ids", "2,3"))
                 .andDo(MockMvcResultHandlers.print()).andReturn();
-        Assert.assertEquals(HttpServletResponse.SC_OK, result.getResponse().getStatus());
+        Assert.assertEquals(HttpServletResponse.SC_BAD_REQUEST, result.getResponse().getStatus());
     }
 }
