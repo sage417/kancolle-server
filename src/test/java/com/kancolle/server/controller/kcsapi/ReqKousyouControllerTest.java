39e594868a12fb9d7b63ac862d56e64a9d5babc2
J.K.SAGE <J.K.SAGE@JKSAGE-PC>
2015-07-15 18:22:40 +0800

    Fix:单元测试开始覆盖

--- src/test/java/com/kancolle/server/controller/kcsapi/ReqKousyouControllerTest.java
+++ src/test/java/com/kancolle/server/controller/kcsapi/ReqKousyouControllerTest.java
@@ -5,7 +5,9 @@ package com.kancolle.server.controller.kcsapi;
 
 import static org.junit.Assert.fail;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.sql.SQLException;
 
@@ -15,6 +17,7 @@ import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +28,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.context.WebApplicationContext;
@@ -39,9 +41,15 @@ import com.alibaba.druid.pool.DruidDataSource;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration(value = "src/main/webapp")
-@ContextHierarchy({ @ContextConfiguration(name = "parent", locations = "classpath*:/spring/spring-context.xml"),
-        @ContextConfiguration(name = "child", locations = "classpath*:/spring/mvc-context.xml") })
+@ContextHierarchy({ @ContextConfiguration(name = "parent", locations = "classpath*:/spring/spring-context.xml"), @ContextConfiguration(name = "child", locations = "classpath*:/spring/mvc-context.xml") })
 public class ReqKousyouControllerTest {
+    private static Reader scriptReader;
+
+    @BeforeClass
+    public static void prepare() throws FileNotFoundException, IOException {
+        scriptReader = Files.newBufferedReader(ResourceUtils.getFile("classpath:kancolle-data.sql").toPath());
+    }
+
     @Autowired
     DruidDataSource dataSource;
 
@@ -56,9 +64,10 @@ public class ReqKousyouControllerTest {
     }
 
     @After
-    public void restoreDb() throws SQLException, IOException {
+    public void restoreDb() throws SQLException {
         ScriptRunner runner = new ScriptRunner(dataSource.getConnection());
-        runner.runScript(Files.newBufferedReader(ResourceUtils.getFile("classpath:kancolle.sql").toPath()));
+        runner.setLogWriter(null);
+        runner.runScript(scriptReader);
         runner.closeConnection();
     }
 
@@ -72,9 +81,7 @@ public class ReqKousyouControllerTest {
 
     @Test
     public void testDestroyitem2() throws Exception {
-        MvcResult result = mockMvc
-                .perform(MockMvcRequestBuilders.post("/kcsapi/api_req_kousyou/destroyitem2").param("api_token", "de1d61f922ae5604a0c479914813d8a18d5c9b6f").param("api_slotitem_ids", "2,3"))
-                .andDo(MockMvcResultHandlers.print()).andReturn();
-        Assert.assertEquals(HttpServletResponse.SC_BAD_REQUEST, result.getResponse().getStatus());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/kcsapi/api_req_kousyou/destroyitem2").param("api_token", "de1d61f922ae5604a0c479914813d8a18d5c9b6f").param("api_slotitem_ids", "2,3")).andReturn();
+        Assert.assertEquals(HttpServletResponse.SC_OK, result.getResponse().getStatus());
     }
 }
