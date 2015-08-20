33ef9ed3b7f71d44f599875bee98de8ef51b857e
J.K.SAGE <J.K.SAGE@JKSAGE-PC>
2015-08-20 22:51:57 +0800

    Update:api list

--- src/main/java/com/kancolle/server/service/battle/impl/MapBattleServiceImpl.java
+++ src/main/java/com/kancolle/server/service/battle/impl/MapBattleServiceImpl.java
@@ -3,6 +3,8 @@
  */
 package com.kancolle.server.service.battle.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.kancolle.server.controller.kcsapi.battle.form.MapStartForm;
 import com.kancolle.server.model.kcsapi.battle.MapStartResult;
 import com.kancolle.server.service.battle.MapBattleService;
@@ -12,6 +14,7 @@ import com.kancolle.server.service.battle.MapBattleService;
  * @Date 2015年8月20日
  *
  */
+@Service
 public class MapBattleServiceImpl implements MapBattleService {
 
     @Override
