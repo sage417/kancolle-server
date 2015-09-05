a91fe4317fdca912b36b8cdd4f2291eca7d72826
sage417 <example@foxmail.com>
2015-09-05 20:37:21 +0800

    Update:battle result Remove:unuse logic

--- src/main/java/com/kancolle/server/logic/ShipLogic.java
+++
@@ -1,9 +0,0 @@
-package com.kancolle.server.logic;
-
-public class ShipLogic {
-
-    public static boolean checkDestory(String member_id, long api_ship_id) {
-        // 入渠不能，远征不能，出击不能，第一舰队旗舰不能，只剩下一艘不能，加锁不能，装备加锁装备不能。。。
-        return false;
-    }
-}
