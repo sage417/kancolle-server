9d366368ea48d81c9eef953835ceb284f8c3e95a
sage417 <sage417@foxmail.com>
2016-03-10 10:54:56 +0800

    update jackson test

--- src/test/java/com/kancolle/server/jackjson/JackJsonTest.java
+++ src/test/java/com/kancolle/server/jackjson/JackJsonTest.java
@@ -1,24 +1,43 @@
 /**
- * 
+ *
  */
 package com.kancolle.server.jackjson;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kancolle.server.model.kcsapi.useitem.UseItemResult;
-import com.kancolle.server.model.po.useitem.UseItem;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author J.K.SAGE
  * @Date 2015年11月6日
- *
  */
 public class JackJsonTest {
+
+    @Test
+    public void testReadArrayNode() throws IOException {
+        String arrStr = "[1,2,3]";
+        ObjectMapper mapper = new ObjectMapper();
+        List<Long> longList = mapper.readValue(arrStr, mapper.getTypeFactory().constructCollectionType(List.class, Long.class));
+        long[] result = mapper.convertValue(longList, long[].class);
+        Assert.assertArrayEquals(new long[]{1L, 2L, 3L}, result);
+    }
+
+    @Test
+    public void testUnicode() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+        A a = new A("中文", 14, 120, 60);
+        String out = objectMapper.writeValueAsString(a);
+        Assert.assertEquals("{\"age\":14,\"height\":120,\"weight\":60,\"NAME\":\"\\u4E2D\\u6587\"}", out);
+    }
 
     @Test
     public void test() {
@@ -26,7 +45,7 @@ public class JackJsonTest {
         ObjectMapper om = new ObjectMapper();
         try {
             String out = om.writeValueAsString(a);
-            Assert.assertEquals("{\"age\":14,\"height\":120,\"weight\":60,\"NAME\":\"sage\"}",out);
+            Assert.assertEquals("{\"age\":14,\"height\":120,\"weight\":60,\"NAME\":\"sage\"}", out);
         } catch (JsonProcessingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -38,12 +57,60 @@ public class JackJsonTest {
         UseItemResult useItem = new UseItemResult();
         ObjectMapper om = new ObjectMapper();
         System.out.print(om.writeValueAsString(useItem));
+    }
 
+    @Test
+    public void testIndex() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        B b = new B();
+        b.setAge(1);
+        b.setHeight(2);
+        b.setName("name");
+        String out = mapper.writeValueAsString(b);
+
+        System.out.println(out);
 
 
     }
 
-    @JsonPropertyOrder(value = {  "age", "height", "weight","name" })
+
+    private static class B {
+
+        @JsonProperty(index = 3)
+        private String name;
+
+        @JsonProperty(index = 2)
+        private int age;
+
+        @JsonProperty(index = 1)
+        private int height;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+    }
+
+    @JsonPropertyOrder(value = {"age", "height", "weight", "name"})
     private static class A {
 
         @JsonProperty("NAME")
