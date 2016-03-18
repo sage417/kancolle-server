4eac0c6417e416e9f3520d64ba6a3a0c33a1ace8
sage417 <sage417@foxmail.com>
2016-03-18 15:51:22 +0800

    udpate controller method name

--- src/main/java/com/kancolle/server/controller/kcsapi/StartController.java
+++ src/main/java/com/kancolle/server/controller/kcsapi/StartController.java
@@ -1,13 +1,12 @@
 package com.kancolle.server.controller.kcsapi;
 
+import com.kancolle.server.model.kcsapi.start.StartModel;
+import com.kancolle.server.model.response.APIResponse;
+import com.kancolle.server.service.start.StartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.kancolle.server.model.kcsapi.start.StartModel;
-import com.kancolle.server.model.response.APIResponse;
-import com.kancolle.server.service.start.StartService;
 
 @RestController
 @RequestMapping(value = "/kcsapi", method = RequestMethod.POST)
@@ -16,14 +15,8 @@ public class StartController {
     @Autowired
     private StartService startService;
 
-    @Deprecated
-    @RequestMapping("/api_start")
-    public String Start() {
-        return "";
-    }
-
     @RequestMapping("/api_start2")
-    public APIResponse<StartModel> Start2() throws Exception {
+    public APIResponse<StartModel> start2() throws Exception {
         StartModel api_data = startService.getStartModel();
         return new APIResponse<StartModel>().setApi_data(api_data);
     }
