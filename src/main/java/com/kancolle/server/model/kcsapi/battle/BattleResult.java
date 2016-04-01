0b026cc95924521667cf7f2f4b89074dc1eeadf7
sage417 <sage417@foxmail.com>
2016-04-01 20:09:29 +0800

    update battle result

--- src/main/java/com/kancolle/server/model/kcsapi/battle/BattleResult.java
+++ src/main/java/com/kancolle/server/model/kcsapi/battle/BattleResult.java
@@ -3,6 +3,7 @@
  */
 package com.kancolle.server.model.kcsapi.battle;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -23,72 +24,188 @@ import java.util.List;
         "api_ship_id", "api_win_rank", "api_get_exp", "api_mvp",
         "api_member_lv", "api_member_exp", "api_get_base_exp", "api_get_ship_exp",
         "api_get_exp_lvup", "api_dests", "api_destsf", "api_lost_flag",
-        "api_quest_name", "api_quest_level", "api_enemy_info", "api_first_clear", "api_mapcell_incentive",
-        "api_get_flag", "api_get_useitem", "api_get_ship", "api_get_slotitem",
-        "api_get_eventitem", "api_get_eventflag", "api_get_exmap_rate", "api_get_exmap_useitem_id"})
+        "api_quest_name", "api_quest_level", "api_enemy_info", "api_first_clear",
+        "api_mapcell_incentive", "api_get_flag", "api_get_useitem", "api_get_ship",
+        "api_get_slotitem", "api_get_eventitem", "api_get_eventflag", "api_get_exmap_rate",
+        "api_get_exmap_useitem_id", "api_landing_hp"
+})
 public class BattleResult {
+
+    public static final int EXMAP_USEITEM_ID = 57;
+
+    public static final int GET_NONE = 0;
+
+    public static final int GET_USEITEM = 1;
+
+    public static final int GET_SHIP = 2;
+
+    public static final int GET_SLOTITEM = 4;
+
+    /* ---------------- 缓存 --------------- */
+
+    private static final int[] GET_FLAG_ARR_NONE = {0, 0, 0};
+
+    private static final int[] GET_FLAG_ARR_USEITEM = {1, 0, 0};
+    private static final int[] GET_FLAG_ARR_SHIP = {0, 1, 0};
+    private static final int[] GET_FLAG_ARR_SLOTITEM = {0, 0, 1};
+
+    private static final int[] GET_FLAG_ARR_USEITEM_AND_SHIP = {1, 1, 0};
+    private static final int[] GET_FLAG_ARR_USEITEM_AND_SLOTITEM = {1, 0, 1};
+    private static final int[] GET_FLAG_ARR_SHIP_AND_SLOTITEM = {0, 1, 1};
+
+    private static final int[] GET_FLAG_ARR_ALL = {1, 1, 1};
+
+    /* ---------------- 缓存 --------------- */
+
 
     /* 敌舰队Id */
     @JsonProperty(value = "api_ship_id")
+    @JSONField(name = "api_ship_id", ordinal = 1)
     private long[] ship_id;
 
     @JsonProperty(value = "api_win_rank")
+    @JSONField(name = "api_win_rank", ordinal = 2)
     private int win_rank;
 
     @JsonProperty(value = "api_get_exp")
+    @JSONField(name = "api_get_exp", ordinal = 3)
     /* 提督获取exp */
     private int get_exp;
 
     @JsonProperty(value = "api_mvp")
+    @JSONField(name = "api_mvp", ordinal = 4)
     /* mvp 索引  从1开始 -1表示不存在 */
     private int mvp;
 
+    @JsonProperty(value = "api_member_lv")
+    @JSONField(name = "api_member_lv", ordinal = 5)
     private int member_lv;
 
+    @JsonProperty(value = "api_member_exp")
+    @JSONField(name = "api_member_exp", ordinal = 6)
     private long member_exp;
 
+    @JsonProperty(value = "api_get_base_exp")
+    @JSONField(name = "api_get_base_exp", ordinal = 7)
     /* 基本经验值 */
     private int base_exp;
 
+    @JsonProperty(value = "api_ship_exp")
+    @JSONField(name = "api_ship_exp", ordinal = 8)
     /* 各舰获得经验值 */
     private int[] ship_exp;
 
+    @JsonProperty(value = "api_get_exp_lvup")
+    @JSONField(name = "api_get_exp_lvup", ordinal = 9)
+    /* 各艦の[0]獲得前経験値, [1]次のレベルの経験値(Lv99|150の場合存在せず), (レベルアップしたなら)[2]その次のレベルの経験値, ... */
     private long[][] exp_lvup;
 
+    @JsonProperty(value = "api_dests")
+    @JSONField(name = "api_dests", ordinal = 10)
+    /* 敵艦撃沈数 */
     private int dests;
 
+    @JsonProperty(value = "api_destsf")
+    @JSONField(name = "api_destsf", ordinal = 11)
+    /* 旗艦撃沈フラグ */
     private int destsf;
 
+    @JsonProperty(value = "api_lost_flag")
+    @JSONField(name = "api_lost_flag", ordinal = 12)
+    /* 味方艦撃沈フラグ？-1から始まる */
     private int lost_flag;
 
+    @JsonProperty(value = "api_quest_name")
+    @JSONField(name = "api_quest_name", ordinal = 13)
     private String quest_name;
 
+    @JsonProperty(value = "api_quest_level")
+    @JSONField(name = "api_quest_level", ordinal = 14)
     private int quest_level;
 
+    @JsonProperty(value = "api_enemy_info")
+    @JSONField(name = "api_enemy_info", ordinal = 15)
     private List<EnemyInfo> enemy_info;
 
+    @JsonProperty(value = "api_first_clear")
+    @JSONField(name = "api_first_clear", ordinal = 16)
+    /* 初回クリアフラグ(EO海域攻略時も1) */
     private int first_clear;
 
+    @JsonProperty(value = "api_mapcell_incentive")
+    @JSONField(name = "api_mapcell_incentive", ordinal = 17)
+    /* 航空偵察作戦報酬獲得フラグ　0=なし, 1=獲得 */
     private int mapcell_incentive;
 
-    private int get_flag;
+    @JsonProperty(value = "api_get_flag")
+    @JSONField(name = "api_get_flag", ordinal = 18)
+    /* 入手フラグ [0]=アイテム, [1]=艦娘 */
+    private int[] get_flag;
 
-    private List<GetItem> get_item;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(value = "api_get_useitem")
+    @JSONField(name = "api_get_useitem", ordinal = 19)
+    private List<GetItem> get_useitem;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(value = "api_get_ship")
+    @JSONField(name = "api_get_ship", ordinal = 20)
     private List<GetShip> get_ship;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(value = "api_get_slotitem")
+    @JSONField(name = "api_get_slotitem", ordinal = 21)
     private List<GetSlotItem> get_slotitem;
 
-    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(value = "api_get_eventitem")
+    @JSONField(name = "api_get_eventitem", ordinal = 22)
     private List<GetEventItem> get_eventitem;
 
-    private int eventflag;
+    @JsonProperty(value = "api_get_eventflag")
+    @JSONField(name = "api_get_eventflag", ordinal = 23)
+    private Integer eventflag;
 
+    @JsonProperty(value = "api_get_exmap_rate")
+    @JSONField(name = "api_get_exmap_rate", ordinal = 24)
+    /* EO海域攻略時：獲得戦果(文字列) それ以外は0(数値) */
     private int exmap_rate;
 
+    @JsonProperty(value = "api_get_exmap_useitem_id")
+    @JSONField(name = "api_get_exmap_useitem_id", ordinal = 25)
+    /* EO海域攻略時：取得アイテムID(文字列) "57"=勲章　それ以外は0(数値) */
     private int exmap_useitem_id;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(value = "api_landing_hp")
+    @JSONField(name = "api_landing_hp", ordinal = 26)
     private LandingHp landing_hp;
+
+    public BattleResult() {
+    }
+
+    public void setGetFlagNone(int getFlag) {
+        switch (getFlag) {
+            case GET_NONE:
+                this.get_flag =  GET_FLAG_ARR_NONE;
+            case GET_USEITEM:
+                this.get_flag = GET_FLAG_ARR_USEITEM;
+            case GET_SHIP:
+                this.get_flag = GET_FLAG_ARR_SHIP;
+            case GET_SLOTITEM:
+                this.get_flag = GET_FLAG_ARR_SLOTITEM;
+            case GET_USEITEM | GET_SHIP:
+                this.get_flag = GET_FLAG_ARR_USEITEM_AND_SHIP;
+            case GET_USEITEM | GET_SLOTITEM:
+                this.get_flag = GET_FLAG_ARR_USEITEM_AND_SLOTITEM;
+            case GET_SHIP | GET_SLOTITEM:
+                this.get_flag = GET_FLAG_ARR_SHIP_AND_SLOTITEM;
+            case GET_USEITEM | GET_SHIP | GET_SLOTITEM:
+                this.get_flag = GET_FLAG_ARR_ALL;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 
     public long[] getShip_id() {
         return ship_id;
@@ -226,20 +343,20 @@ public class BattleResult {
         this.mapcell_incentive = mapcell_incentive;
     }
 
-    public int getGet_flag() {
+    public int[] getGet_flag() {
         return get_flag;
     }
 
-    public void setGet_flag(int get_flag) {
+    public void setGet_flag(int[] get_flag) {
         this.get_flag = get_flag;
     }
 
-    public List<GetItem> getGet_item() {
-        return get_item;
+    public List<GetItem> getGet_useitem() {
+        return get_useitem;
     }
 
-    public void setGet_item(List<GetItem> get_item) {
-        this.get_item = get_item;
+    public void setGet_useitem(List<GetItem> get_useitem) {
+        this.get_useitem = get_useitem;
     }
 
     public List<GetShip> getGet_ship() {
@@ -266,11 +383,11 @@ public class BattleResult {
         this.get_eventitem = get_eventitem;
     }
 
-    public int getEventflag() {
+    public Integer getEventflag() {
         return eventflag;
     }
 
-    public void setEventflag(int eventflag) {
+    public void setEventflag(Integer eventflag) {
         this.eventflag = eventflag;
     }
 
