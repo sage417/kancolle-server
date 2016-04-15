9a64a01a21222e7a609004d7be40ca575dcf0fbe
sage417 <sage417@foxmail.com>
2016-04-15 17:35:46 +0800

    update MemberMapBattleMapper

--- src/main/java/com/kancolle/server/mapper/map/MemberMapBattleMapper.java
+++ src/main/java/com/kancolle/server/mapper/map/MemberMapBattleMapper.java
@@ -13,6 +13,13 @@ import org.apache.ibatis.annotations.Param;
  */
 public interface MemberMapBattleMapper {
 
+    String MAPCELL_ID = "mapCellId";
+    String GET_RECOURCE = "getRecource";
+    String FETCH_RECOURCE = "fetchRecource";
+    String BATTLE_FLAG = "battleFlag";
+    String RESULT_FLAG = "resultFlag";
+    String SESSION = "session";
+
     void insertMemberMapBattleState(@Param("member_id") String member_id, @Param("deck_id") Integer deck_id, @Param("traveller_No") int traveller_No, @Param("map_no") int map_no);
 
     MemberMapBattleState selectMemberMapBattleState(@Param("member_id") String member_id);
