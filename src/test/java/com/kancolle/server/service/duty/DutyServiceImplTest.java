602b6511d4b950cfc249d969ce751ef3452a5b1f
sage417 <sage417@foxmail.com>
2016-04-18 00:26:16 +0900

    no...

--- src/test/java/com/kancolle/server/service/duty/DutyServiceImplTest.java
+++ src/test/java/com/kancolle/server/service/duty/DutyServiceImplTest.java
@@ -1,15 +1,14 @@
 package com.kancolle.server.service.duty;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.kancolle.server.model.po.duty.Duty;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -25,13 +24,12 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
         @ContextConfiguration(name = "parent", locations = "classpath:spring/spring-context.xml"),
         @ContextConfiguration(name = "child", locations = "classpath:spring/spring-mvc.xml")
 })
+@Sql(value = {"classpath:sql/kancolle-schema.sql", "classpath:sql/kancolle-data.sql"})
 public class DutyServiceImplTest {
 
     @Autowired
     DutyService dutyService;
 
-    @Test
-    @DatabaseSetup("/dbunit/duty/duty_data.xml")
     public void test() {
         Duty duty = dutyService.getDutyByNo(701);
         Assert.assertNotNull(duty);
--- src/test/java/com/kancolle/server/service/ship/impl/MemberShipServiceTest.java
+++ src/test/java/com/kancolle/server/service/ship/impl/MemberShipServiceTest.java
@@ -1,8 +1,6 @@
 package com.kancolle.server.service.ship.impl;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DatabaseSetups;
 import com.kancolle.server.model.po.ship.MemberShip;
 import com.kancolle.server.service.ship.MemberShipService;
 import org.junit.Test;
@@ -11,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -25,14 +24,12 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
         @ContextConfiguration(name = "parent", locations = "classpath:spring/spring-context.xml"),
         @ContextConfiguration(name = "child", locations = "classpath:spring/spring-mvc.xml")
 })
+@Sql(value = {"classpath:sql/kancolle-schema.sql", "classpath:sql/kancolle-data.sql"})
 public class MemberShipServiceTest {
     @Autowired
     private MemberShipService service;
 
     @Test
-    @DatabaseSetups({@DatabaseSetup("/dbunit/membership/membership_data.xml"),
-            @DatabaseSetup("/dbunit/membership/ship_level_data.xml"),
-            @DatabaseSetup("/dbunit/membership/ship_data.xml")})
     public void test() {
         MemberShip mship = service.getMemberShip("8006690", 1L);
         service.increaseMemberShipExp(mship, 1);
