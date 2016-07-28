d374febe7715f60bd3e68dcac0acf55ce366ac8d
sage417 <sage417@foxmail.com>
2016-07-28 17:21:11 +0800

    update service-context

--- src/main/java/com/kancolle/server/config/ServiceBeans.java
+++ src/main/java/com/kancolle/server/config/ServiceBeans.java
@@ -1,8 +1,13 @@
 package com.kancolle.server.config;
 
 import com.google.common.eventbus.EventBus;
+import com.kancolle.server.utils.SpringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
+
+import static com.fasterxml.jackson.core.JsonGenerator.Feature.ESCAPE_NON_ASCII;
+import static com.fasterxml.jackson.databind.MapperFeature.DEFAULT_VIEW_INCLUSION;
 
 /**
  * Package: com.kancolle.server.config
@@ -15,5 +20,17 @@ public class ServiceBeans {
     @Bean(name = "dutyBus")
     public EventBus getDutyEventBus() {
         return new EventBus();
+    }
+
+    @Bean(name = "springUtils")
+    public SpringUtils getSpringUtils() {
+        return new SpringUtils();
+    }
+
+    @Bean(name = "jackson2ObjectMapper")
+    public Jackson2ObjectMapperFactoryBean getJackson2ObjectMapperFactoryBean() {
+        Jackson2ObjectMapperFactoryBean factoryBean = new Jackson2ObjectMapperFactoryBean();
+        factoryBean.setFeaturesToEnable(ESCAPE_NON_ASCII, DEFAULT_VIEW_INCLUSION);
+        return factoryBean;
     }
 }
