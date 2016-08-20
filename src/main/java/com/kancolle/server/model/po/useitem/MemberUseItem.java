8628d074ad3bf5bcf31d0b375b3de0b1c2565abd
sage417 <sage417@foxmail.com>
2016-08-20 15:06:36 +0800

    update member useItem

--- src/main/java/com/kancolle/server/model/po/useitem/MemberUseItem.java
+++ src/main/java/com/kancolle/server/model/po/useitem/MemberUseItem.java
@@ -1,16 +1,16 @@
 package com.kancolle.server.model.po.useitem;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.common.collect.ImmutableList;
 import org.apache.ibatis.type.Alias;
 
 import java.io.Serializable;
 
 @JsonPropertyOrder(value = {
-        "member_id", "useItemId", "value", "usetype",
-        "category", "name", "description", "price",
+        "member_id", "useItemId", "value", "api_usetype",
+        "api_category", "api_name", "api_description", "api_price",
         "count"
 })
 @Alias("MemberUseItem")
@@ -19,105 +19,43 @@ public class MemberUseItem implements Serializable {
     private static final long serialVersionUID = 3773472417961605052L;
 
     @JsonProperty(value = "api_member_id")
-    @JSONField(ordinal = 1, name = "api_member_id")
-    private long member_id;
+    private final long member_id;
 
     @JsonProperty(value = "api_id")
-    @JSONField(ordinal = 2, name = "api_id")
-    private int useItemId;
+    private final int useItemId;
 
     @JsonProperty(value = "api_value")
-    @JSONField(ordinal = 3, name = "api_value")
-    public int value;
+    public final int value;
 
-    @JsonProperty(value = "api_usetype")
-    @JSONField(ordinal = 4, name = "api_usetype")
-    private int usetype;
-
-    @JsonProperty(value = "api_category")
-    @JSONField(ordinal = 5, name = "api_category")
-    private int category;
-
-    @JsonProperty(value = "api_name")
-    @JSONField(ordinal = 6, name = "api_name")
-    private String name;
+    @JsonIgnore
+    private final UseItem useItem;
 
     @JsonProperty(value = "api_description")
-    @JSONField(ordinal = 7, name = "api_description")
-    private JSONArray description;
-
-    @JsonProperty(value = "api_price")
-    @JSONField(ordinal = 8, name = "api_price")
-    private int price;
+    private final ImmutableList<String> description = ImmutableList.of("","");
 
     @JsonProperty(value = "api_count")
-    @JSONField(ordinal = 9, name = "api_count")
     private int count;
+
+    public MemberUseItem(final long member_id, final int useItemId, final int value, final UseItem useItem) {
+        this.member_id = member_id;
+        this.useItemId = useItemId;
+        this.value = value;
+        this.useItem = useItem;
+    }
 
     public long getMember_id() {
         return member_id;
-    }
-
-    public void setMember_id(long member_id) {
-        this.member_id = member_id;
-    }
-
-    public int getUseItemId() {
-        return useItemId;
-    }
-
-    public void setUseItemId(int useItemId) {
-        this.useItemId = useItemId;
     }
 
     public int getValue() {
         return value;
     }
 
-    public void setValue(int value) {
-        this.value = value;
+    public int getUseItemId() {
+        return useItemId;
     }
 
-    public int getUsetype() {
-        return usetype;
-    }
-
-    public void setUsetype(int usetype) {
-        this.usetype = usetype;
-    }
-
-    public int getCategory() {
-        return category;
-    }
-
-    public void setCategory(int category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public JSONArray getDescription() {
-        return description;
-    }
-
-    public void setDescription(JSONArray description) {
-        this.description = description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
+    @JsonProperty(value = "api_count")
     public int getCount() {
         return count;
     }
@@ -126,33 +64,64 @@ public class MemberUseItem implements Serializable {
         this.count = count;
     }
 
+    @JsonProperty(value = "api_usetype")
+    public int getUsetype() {
+        return useItem.getUsetype();
+    }
+
+    @JsonProperty(value = "api_category")
+    public int getCategory() {
+        return useItem.getCategory();
+    }
+
+    @JsonProperty(value = "api_name")
+    public String getName() {
+        return useItem.getName();
+    }
+
+    public ImmutableList<String> getDescription() {
+        return description;
+    }
+
+    @JsonProperty(value = "api_price")
+    public int getPrice() {
+        return useItem.getPrice();
+    }
+
+    public UseItem getUseItem() {
+        return useItem;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MemberUseItem that = (MemberUseItem) o;
+
+        if (member_id != that.member_id) return false;
+        if (useItemId != that.useItemId) return false;
+        if (value != that.value) return false;
+        return count == that.count;
+
+    }
+
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (int) (member_id ^ (member_id >>> 32));
-        result = prime * result + useItemId;
+        int result = (int) (member_id ^ (member_id >>> 32));
+        result = 31 * result + useItemId;
+        result = 31 * result + value;
+        result = 31 * result + count;
         return result;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        MemberUseItem other = (MemberUseItem) obj;
-        if (member_id != other.member_id)
-            return false;
-        if (useItemId != other.useItemId)
-            return false;
-        return true;
-    }
-
-    @Override
     public String toString() {
-        return "MemberUseItem [member_id=" + member_id + ", useItemId=" + useItemId + ", count=" + count + "]";
+        final StringBuilder sb = new StringBuilder("MemberUseItem{");
+        sb.append("member_id=").append(member_id);
+        sb.append(", useItem=").append(useItem);
+        sb.append(", count=").append(count);
+        sb.append('}');
+        return sb.toString();
     }
 }
