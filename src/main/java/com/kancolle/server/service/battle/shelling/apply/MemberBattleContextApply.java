c12d5eed46990dea6e8720f6e7d6984d45479739
sage417 <sage417@foxmail.com>
2016-09-07 19:55:00 +0800

    update shelling

---
+++ src/main/java/com/kancolle/server/service/battle/shelling/apply/MemberBattleContextApply.java
@@ -0,0 +1,30 @@
+package com.kancolle.server.service.battle.shelling.apply;
+
+import com.kancolle.server.model.po.battle.BattleContext;
+import com.kancolle.server.model.po.ship.IShip;
+import com.kancolle.server.utils.logic.battle.BattleContextUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by SAGE on 16/9/7.
+ */
+@Component
+public class MemberBattleContextApply implements BattleContextApply
+{
+    @Override
+    public int getCurrentAerialState(BattleContext context) {
+        return context.getMemberAerialState();
+    }
+
+    @Override
+    public int getCurrentFormation(BattleContext context) {
+        return BattleContextUtils.getMemberFormation(context);
+    }
+
+    @Override
+    public List<? extends IShip> getCurrentShips(BattleContext context) {
+        return context.getMemberDeckPort().getShips();
+    }
+}
---
+++ src/main/java/com/kancolle/server/service/battle/shelling/apply/UnderSeaBattleContextApply.java
@@ -0,0 +1,29 @@
+package com.kancolle.server.service.battle.shelling.apply;
+
+import com.kancolle.server.model.po.battle.BattleContext;
+import com.kancolle.server.model.po.ship.IShip;
+import com.kancolle.server.utils.logic.battle.BattleContextUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by SAGE on 16/9/7.
+ */
+@Component
+public class UnderSeaBattleContextApply implements BattleContextApply{
+    @Override
+    public int getCurrentAerialState(BattleContext context) {
+        return context.getUnderSeaSakuteki();
+    }
+
+    @Override
+    public int getCurrentFormation(BattleContext context) {
+        return BattleContextUtils.getUnderSeaFormation(context);
+    }
+
+    @Override
+    public List<? extends IShip> getCurrentShips(BattleContext context) {
+        return context.getUnderSeaDeckPort().getUnderSeaShips();
+    }
+}
