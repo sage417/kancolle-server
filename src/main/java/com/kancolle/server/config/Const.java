9011d0e5f7f2d719f4a8381236543eef0b6c15fc
175405 <175405@sh.lianjia.com>
2016-12-07 18:43:11 +0800

    update const

--- src/main/java/com/kancolle/server/config/Const.java
+++ src/main/java/com/kancolle/server/config/Const.java
@@ -14,27 +14,11 @@ public class Const {
 
     @Bean(name = "useItemIds")
     public ImmutableList<Integer> createUseItemIds() {
-        return new ImmutableList.Builder<Integer>()
-                .add(10)
-                .add(11)
-                .add(12)
-                .add(52)
-                .add(54)
-                .add(55)
-                .add(56)
-                .add(57)
-                .add(58)
-                .add(59)
-                .add(60)
-                .add(61)
-                .add(62)
-                .add(63)
-                .build();
-        //return new int[]{10, 11, 12, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63};
+        return ImmutableList.of(10, 11, 12, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63);
     }
 
     @Bean(name = "equip_exslot")
     public ImmutableList<Integer> equip_exslot() {
-        return new ImmutableList.Builder<Integer>().add(23).add(43).add(44).build();
+        return ImmutableList.of(23, 43, 44);
     }
 }
