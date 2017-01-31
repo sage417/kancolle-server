a4135a7951436a12ad0c5f304604ce28f7646d3e
sage417 <sage417@foxmail.com>
2017-01-31 23:09:22 +0800

    update spring boot test

--- src/test/java/com/kancolle/server/controller/kcsapi/BattleControllerTest.java
+++ src/test/java/com/kancolle/server/controller/kcsapi/BattleControllerTest.java
@@ -3,24 +3,17 @@
  */
 package com.kancolle.server.controller.kcsapi;
 
-import com.kancolle.server.KancolleApp;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -28,21 +21,13 @@ import javax.servlet.http.HttpServletResponse;
  * @author J.K.SAGE
  * @Date 2015年7月13日
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = KancolleApp.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @Sql(value = {"classpath:sql/kancolle-dump.sql"})
 public class BattleControllerTest {
-
     @Autowired
-    private WebApplicationContext wac;
-
     private MockMvc mockMvc;
-
-    @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
 
     @Test
     public void testMapTraveller() throws Exception {
