ab58f7c2d37f0ed6db0f9aaefd69364e1b89b63c
sage417 <sage417@foxmail.com>
2017-03-12 15:12:41 +0800

    update httpclient config

--- src/main/java/com/kancolle/server/config/ServiceConfig.java
+++ src/main/java/com/kancolle/server/config/ServiceConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.common.eventbus.EventBus;
 import com.kancolle.server.utils.SpringUtils;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,8 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.fasterxml.jackson.core.JsonGenerator.Feature.ESCAPE_NON_ASCII;
 import static com.fasterxml.jackson.databind.MapperFeature.DEFAULT_VIEW_INCLUSION;
@@ -50,6 +53,17 @@ public class ServiceConfig {
         HttpClient httpClient = HttpClientBuilder
                 .create()
                 .disableCookieManagement()
+                .disableAutomaticRetries()
+                .disableRedirectHandling()
+                .setDefaultRequestConfig(
+                        RequestConfig.custom()
+                                .setConnectTimeout(400)
+                                .setSocketTimeout(2 * 1000)
+                                .setConnectionRequestTimeout(200)
+                                .build())
+                .setMaxConnTotal(1000)
+                .setMaxConnPerRoute(500)
+                .setConnectionTimeToLive(1L, TimeUnit.MINUTES)
                 .build();
         ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(requestFactory);
